2ba9d49a3c4706686969810493b8a3712db0b17b
victorinojohnmark <victorinojohnmark@gmail.com>
2023-12-21 15:37:58 +0800

    Auth Scaffolding and Assets added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+import '../css/app.css'
+
 import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+// Import Components
+import ExampleComponent from './components/ExampleComponent.vue';
+
+// Register Components
+app.component('example-component', ExampleComponent);
+app.mount('#app');
