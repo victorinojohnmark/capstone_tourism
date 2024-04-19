f2ef8a53a9777d61b26acc24d7ac59340ba33f24
victorinojohnmark <victorinojohnmark@gmail.com>
2024-04-19 11:41:15 +0800

    Update carousel

--- resources/views/user/vendor/view.blade.php
+++ resources/views/user/vendor/view.blade.php
@@ -49,12 +49,10 @@
         <div class="relative block">
             <hr class="block my-3">
             <h3 class="text-gray-700 text-xl my-3">Gallery</h3>
-            <div class="columns-2 md:columns-3 lg:columns-4 mt-6">
+            {{-- <div class="columns-2 md:columns-3 lg:columns-4 mt-6">
                 @forelse ($user->galleries as $gallery)
                 <div class="relative">
-                    
-                    <img class="mb-4 max-w-full hover:cursor-pointer"
-                    src="/storage/gallery/{{ $gallery->filename }}" alt="">
+                    <img class="mb-4 max-w-full hover:cursor-pointer" src="/storage/gallery/{{ $gallery->filename }}" alt="">
                 </div>
                 @empty
                 <div>
@@ -62,7 +60,91 @@
                 </div>
                 @endforelse
                 
-            </div>
+            </div> --}}
+
+            <div class="row">
+                @forelse ($user->galleries as $gallery)
+                    <div class="column">
+                        <img src="/storage/gallery/{{ $gallery->filename }}" class="w-full aspect-video object-cover hover:cursor-pointer" onclick="openModal();currentSlide({{ $loop->index + 1 }})" class="hover-shadow cursor">
+                    </div>
+                @empty
+                <div>
+                    <img src="https://placehold.co/600x400?text=No%20upload%20image%20yet" alt="">
+                </div>
+                @endforelse
+                {{-- <div class="column">
+                  <img src="https://www.w3schools.com/howto/img_nature.jpg" style="width:100%" onclick="openModal();currentSlide(1)" class="hover-shadow cursor">
+                </div>
+                <div class="column">
+                  <img src="https://www.w3schools.com/howto/img_snow.jpg" style="width:100%" onclick="openModal();currentSlide(2)" class="hover-shadow cursor">
+                </div>
+                <div class="column">
+                  <img src="https://www.w3schools.com/howto/img_mountains.jpg" style="width:100%" onclick="openModal();currentSlide(3)" class="hover-shadow cursor">
+                </div>
+                <div class="column">
+                  <img src="https://www.w3schools.com/howto/img_lights.jpg" style="width:100%" onclick="openModal();currentSlide(4)" class="hover-shadow cursor">
+                </div> --}}
+              </div>
+            
+              <div id="myModal" class="modal backdrop-blur-sm bg-gray-800/50">
+                <span class="close cursor" onclick="closeModal()">&times;</span>
+                <div class="modal-content">
+                    
+                    @forelse ($user->galleries as $gallery)
+                        <div class="mySlides">
+                            {{-- <div class="numbertext">1 / 4</div> --}}
+                            <img src="/storage/gallery/{{ $gallery->filename }}" class="w-full aspect-video object-cover">
+                        </div>
+                    @empty
+                    
+                    @endforelse
+                  {{-- <div class="mySlides">
+                    <div class="numbertext">1 / 4</div>
+                    <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width:100%">
+                  </div>
+            
+                  <div class="mySlides">
+                    <div class="numbertext">2 / 4</div>
+                    <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" style="width:100%">
+                  </div>
+            
+                  <div class="mySlides">
+                    <div class="numbertext">3 / 4</div>
+                    <img src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width:100%">
+                  </div>
+            
+                  <div class="mySlides">
+                    <div class="numbertext">4 / 4</div>
+                    <img src="https://www.w3schools.com/howto/img_lights_wide.jpg" style="width:100%">
+                  </div> --}}
+            
+                  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+            
+                  <div class="caption-container">
+                    <p id="caption"></p>
+                  </div>
+
+                  @forelse ($user->galleries as $gallery)
+                    <div class="column">
+                        <img class="demo cursor w-full aspect-video object-cover" src="/storage/gallery/{{ $gallery->filename }}" onclick="currentSlide({{ $loop->index + 1 }})">
+                    </div>
+                    @empty
+                
+                @endforelse
+            
+                  
+                  {{-- <div class="column">
+                    <img class="demo cursor" src="https://www.w3schools.com/howto/img_snow_wide.jpg" style="width:100%" onclick="currentSlide(2)" alt="Snow">
+                  </div>
+                  <div class="column">
+                    <img class="demo cursor" src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                  </div>
+                  <div class="column">
+                    <img class="demo cursor" src="https://www.w3schools.com/howto/img_lights_wide.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+                  </div> --}}
+                </div>
+              </div>
         </div>
     </div>
 </div>
@@ -71,6 +153,53 @@
     <iframe src="{{ $user->map_url }}" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div> --}}
 
+@endsection
+
+@section('scripts')
+    <script>
+        // Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+    </script>
 @endsection
 
 @section('css')
