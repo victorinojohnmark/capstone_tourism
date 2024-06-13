b87c8194c920d37780ca63e154e1d21233fca277
victorinojohnmark <victorinojohnmark@gmail.com>
2024-06-13 21:27:37 +0800

    Account details in modal mode

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -56,6 +56,11 @@
                 {{ $user->business_name ?? '-' }}
             </td> --}}
             <td class="px-6 !py-4">
+                <button data-modal-target="viewAccountModal{{ $user->id }}" data-modal-toggle="viewAccountModal{{ $user->id }}" class="inline mx-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+                    View Details
+                </button>
+                @include('account.account-view-modal')
+
                 <button data-modal-target="deleteAccountModal{{ $user->id }}" data-modal-toggle="deleteAccountModal{{ $user->id }}" class="inline mx-1 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800" type="button">
                     Delete
                 </button>
